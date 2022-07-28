0e09d90258273e3a94b3a64be8b882b07db83d29
VampirSocol <you@example.com>
2022-07-28 21:58:07 +0300

    Refactoring MyServlet

--- src/main/java/com/vampir/MyServlet.java
+++ src/main/java/com/vampir/MyServlet.java
@@ -8,23 +8,11 @@ public class MyServlet extends HttpServlet{
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter writer = response.getWriter();
-		try{
+		try {
 			writer.println("<html><head><body><form action=\"/my-webapp/home/hello\" method=\"POST\">Enter your name: <input name=\"username\" /></form></body></head></html>");
 		} finally {
 			writer.close();
 		}
 	}
-
-	/*protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		PrintWriter writer = resp.getWriter();
-		String name = req.getParameter("username");
-		try{
-			writer.println("<html><h1>Hello " + name + "</h1></html>");
-		} finally {
-			writer.close();
-		}
-		//req.getSession().setAttribute("String", name);
-		//req.getRequestDispatcher("/hello").forward(req, resp);
-	}*/
 	
 } 
